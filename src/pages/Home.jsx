a1f53a0043762d04bf92d20e47826ed83b5390b1
faisal-shohag <faisalshohagprog@gmail.com>
2025-06-01 23:37:55 +0600

    text block component implementation in process

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,17 @@
+import TextBlock from "../components/editor/TextBlock";
 
 const Home = () => {
+
+ 
+
+  return (
+    <div className="max-w-5xl mx-auto">
+      <h1 className="mt-5 text-5xl font-bold">Home</h1>
+
+      <TextBlock/>
     
-    return (
-        <div className="max-w-5xl mx-auto"> 
-          <h1 className="mt-5 text-5xl font-bold">Home</h1>
-        </div>
-    );
+    </div>
+  );
 };
 
 export default Home;
